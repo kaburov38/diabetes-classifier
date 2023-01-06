eca95d1619beb0bd9bb683bee99e28bc543788c9
kaburov38 <kaburov38@gmail.com>
2023-01-06 11:02:15 +0700

    fix wrong prdiction result

--- classifier/views.py
+++ classifier/views.py
@@ -1,7 +1,7 @@
 from email import message
 from pyexpat import model
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 import pickle
@@ -11,34 +11,35 @@ import os
 from django.conf import settings
 import sklearn
 
+with open(os.path.join(settings.AI_MODEL_DIR, 'model.pickle'), "rb") as input_file:
+        model = pickle.load(input_file)
+    
+with open(os.path.join(settings.AI_MODEL_DIR, 'preprocess.pickle'), "rb") as input_file:
+    preprocess = pickle.load(input_file)
+
 # Create your views here.
 def GetForm(request):
     return render(request, 'classifier/index.html')
 
-@require_http_methods(['POST'])
+@require_http_methods(['GET'])
 @csrf_exempt
 def Predict(request):
-    age = request.POST.get('age', 0)
-    sex = request.POST.get('sex', 'Male')
-    polyuria = request.POST.get('polyuria', 'Yes')
-    polydipsia = request.POST.get('polydipsia', 'Yes')
-    sudden_weight_loss = request.POST.get('sudden_weight_loss', 'Yes')
-    weakness = request.POST.get('weakness', 'Yes')
-    polyphagia = request.POST.get('polyphagia', 'Yes')
-    genital_thrush = request.POST.get('genital_thrush', 'Yes')
-    visual_blurring = request.POST.get('visual_blurring', 'Yes')
-    itching = request.POST.get('itching', 'Yes')
-    irritability = request.POST.get('irritability', 'Yes')
-    delayed_healing = request.POST.get('delayed_healing', 'Yes')
-    partial_paresis = request.POST.get('partial_paresis', 'Yes')
-    muscle_stiffness = request.POST.get('muscle_stiffness', 'Yes')
-    alopecia = request.POST.get('alopecia', 'Yes')
-    obesity = request.POST.get('obesity', 'Yes')
-    with open(os.path.join(settings.AI_MODEL_DIR, 'model.pickle'), "rb") as input_file:
-        model = pickle.load(input_file)
-    
-    with open(os.path.join(settings.AI_MODEL_DIR, 'preprocess.pickle'), "rb") as input_file:
-        preprocess = pickle.load(input_file)
+    age = request.GET.get('age', 0)
+    sex = request.GET.get('sex', 'Male')
+    polyuria = request.GET.get('polyuria', 'Yes')
+    polydipsia = request.GET.get('polydipsia', 'Yes')
+    sudden_weight_loss = request.GET.get('sudden_weight_loss', 'Yes')
+    weakness = request.GET.get('weakness', 'Yes')
+    polyphagia = request.GET.get('polyphagia', 'Yes')
+    genital_thrush = request.GET.get('genital_thrush', 'Yes')
+    visual_blurring = request.GET.get('visual_blurring', 'Yes')
+    itching = request.GET.get('itching', 'Yes')
+    irritability = request.GET.get('irritability', 'Yes')
+    delayed_healing = request.GET.get('delayed_healing', 'Yes')
+    partial_paresis = request.GET.get('partial_paresis', 'Yes')
+    muscle_stiffness = request.GET.get('muscle_stiffness', 'Yes')
+    alopecia = request.GET.get('alopecia', 'Yes')
+    obesity = request.GET.get('obesity', 'Yes')
     
     X = pd.DataFrame(
         {
@@ -60,9 +61,9 @@ def Predict(request):
             'Obesity': [obesity]
         }
     )
-    X = preprocess.fit_transform(X)
+    X = preprocess.transform(X)
     result = model.predict(X)
-    message_str = {0: 'negative', 1: 'positive'}
-    message = "Your result is: "+ message_str[result[0]]
-    return HttpResponse(message)
-
+    message_str = {0: 'Negative', 1: 'Positive'}
+    return JsonResponse({
+        'result':  message_str[result[0]]
+    })
